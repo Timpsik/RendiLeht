64c23cb0007c26be0e161db3040b397b6d171250
Timpsik <mikk.ounmaa@gmail.com>
2018-03-13 16:29:01 +0200

    Testkeskkonna vahetus

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = "http://localhost/";
-$config['base_url'] = "http://rendileht.epizy.com/";
+$config['base_url'] = "https://rendileht.netlify.com/";
 
 /*
 |--------------------------------------------------------------------------
