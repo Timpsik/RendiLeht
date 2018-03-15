6e0de557d5a00026a8cb6c3dc5a5fbab177ac4bb
DESKTOP-KOBH4PF\Tarmo R <tarmorii@gmail.com>
2018-03-15 23:53:39 +0200

    no message

---
+++ application/language/English.php
@@ -0,0 +1,35 @@
+<?php
+$lang['Logi_sisse'] = 'Log in';
+$lang['Registreeru'] = 'Register';
+$lang['Rendi_kõike'] = 'Rent everything';
+$lang['Kategooria'] = 'Category';
+$lang['Asukoht'] = 'Location';
+$lang['Algkuupäev'] = 'Start date';
+$lang['Lõppkuupäev'] = 'End date';
+$lang['Sisesta_märksõna'] = 'Search for anything';
+$lang['Otsi'] = 'Search';
+$lang['Top_kuulutused'] = 'Top advertises';
+
+
+$lang['Lisa_kuulutus'] = 'Top kuulutused';
+$lang['Broneeringud'] = 'Reservations';
+$lang['Ajalugu'] = 'History';
+$lang['Minu_kuulutused'] = 'My ads';
+$lang['Minu_andmed'] = 'My profile';
+$lang['Pealkiri'] = 'Title';
+$lang['Kirjeldus'] = 'Description';
+$lang['Telefoni_number'] = 'Phone number';
+$lang['Hind'] = 'Price';õ
+$lang['Tund'] = 'Hour';
+$lang['Päev'] = 'Day';
+$lang['Nädal'] = 'Week';
+$lang['Kuu'] = 'Month';
+$lang['Salvesta'] = 'Save';
+
+
+
+$lang['E-mail'] = 'E-mail';
+$lang['Parool'] = 'Password';
+
+
+
---
+++ application/language/eesti.php
@@ -0,0 +1,29 @@
+<?php
+$lang['Logi_sisse'] = 'Logi sisse';
+$lang['Registreeru'] = 'Registreeru';
+$lang['Rendi_kõike'] = 'Rendi kõike';
+$lang['Kategooria'] = 'Kategooria';
+$lang['Asukoht'] = 'Asukoht';
+$lang['Algkuupäev'] = 'Algkuupäev';
+$lang['Lõppkuupäev'] = 'Lõppkuupäev';
+$lang['Sisesta_märksõna'] = 'Sisesta märksõna';
+$lang['Otsi'] = 'Otsi';
+$lang['Top_kuulutused'] = 'Top kuulutused';
+
+$lang['Lisa_kuulutus'] = 'Lisa kuulutus';
+$lang['Broneeringud'] = 'Broneeringud';
+$lang['Ajalugu'] = 'Ajalugu';
+$lang['Minu_kuulutused'] = 'Minu kuulutused';
+$lang['Minu_andmed'] = 'Minu andmed';
+$lang['Pealkiri'] = 'Pealkiri';
+$lang['Kirjeldus'] = 'Kirjeldus';
+$lang['Telefoni_number'] = 'Telefoni number';
+$lang['Hind'] = 'Hind';
+$lang['Tund'] = 'Tund';
+$lang['Päev'] = 'Päev';
+$lang['Nädal'] = 'Nädal';
+$lang['Kuu'] = 'Kuu';
+$lang['Salvesta'] = 'Salvesta';
+
+$lang['E-mail'] = 'E-mail';
+$lang['Parool'] = 'Parool';
